31b1e8b82bf7afdc4f108d67faec39ca2790ca87
xsalles <xs.salles@gmail.com>
2024-09-19 16:18:46 -0300

    feat: exercise D.

--- src/exD_Pedro.java
+++ src/exD_Pedro.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class exD_Pedro {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] A = new int[15];
+        int[] B = new int[15];
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println("\nDigite um número inteiro (Irá ter essa pergunta 15 vezes)\n");
+            A[i] = sc.nextInt();
+
+            B[i] = A[i] * A[i];
+
+        }
         
+        for (int i = 0; i < 15; i++) {
+            System.out.printf("\n%d ao quadrado é, é %d \n", A[i], B[i]);
+            
+        }
+
     }
 }
---
+++ src/exE_Pedro.java
@@ -0,0 +1,3 @@
+public class exE_Pedro {
+    
+}
