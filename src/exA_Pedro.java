53d86e96f2ddb9b07fcb7fbaa7595a92f4848b63
xsalles <xs.salles@gmail.com>
2024-09-19 14:43:35 -0300

    feat: exercise A.

---
+++ src/exA_Pedro.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class exA_Pedro {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        String[] names = new String[10];
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digite um nome: \n");
+            names[i] = sc.nextLine();
+        }
+
+        System.out.println(Arrays.toString(names));
+    }
+}
